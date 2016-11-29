064a3140f3a2b09d27025baad375a0693d31989d
Yevhen Dubinin <thisiskilling@me.com>
2016-11-29 21:23:12 +0200

    Creates Xcode project to allow standalone build and test the library

---
+++ PeterParkerTests/PeterParkerTests.swift
@@ -0,0 +1,14 @@
+//
+//  PeterParkerTests.swift
+//  PeterParkerTests
+//
+//  Created by Yevhen Dubinin on 11/28/16.
+//  Copyright © 2016 Yevhen Dubinin. All rights reserved.
+//
+
+import XCTest
+@testable import PeterParker
+
+class PeterParkerTests: XCTestCase {
+    
+}
